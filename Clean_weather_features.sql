af4e21f7d2e78fc88cf363e55e4804f16a3a4aaa
Omarhadi79 <123913569+Omarhadi79@users.noreply.github.com>
2023-09-11 20:24:21 -0700

    Add files via upload

--- Clean_weather_features.sql
+++ Clean_weather_features.sql
@@ -1,2 +1,15 @@
-SELECT dt_iso, city_name, temp, temp_min, temp_max, pressure, humidity, wind_speed, wind_deg, rain_1h, rain_3h, snow_3h, clouds_all, weather_main
+SELECT dt_iso, 
+city_name, 
+temp, 
+temp_min, 
+temp_max, 
+pressure, 
+humidity, 
+wind_speed, 
+wind_deg, 
+rain_1h, 
+rain_3h, 
+snow_3h, 
+clouds_all, 
+weather_main
 	FROM public.weather_features;
